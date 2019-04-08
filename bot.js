01a5d494b14960eea66d23465cb35bfa8e1d15b8
isfaa7 <47106531+isfaa7@users.noreply.github.com>
2019-04-08 11:59:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = ""; // ضع البريفكس الخاص بك هنا
+const prefix = "$"; // ضع البريفكس الخاص بك هنا
 
 
 console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
